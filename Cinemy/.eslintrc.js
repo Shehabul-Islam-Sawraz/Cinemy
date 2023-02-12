4dea63827c2576fcfd768b0bcd87b594755df267
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2023-02-12 13:04:12 +0600

    Hello world!

--- Cinemy/.eslintrc.js
+++ Cinemy/.eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'no-console': 'off', // console.log() drops performance. But for debugging, allowing console.log() in our project
+  },
 };
