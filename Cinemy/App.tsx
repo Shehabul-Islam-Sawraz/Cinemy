9b3cdd8006edff59e7394ae80c388438861308b9
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2023-02-12 16:19:42 +0600

    Fetching movies data from API done

--- Cinemy/App.tsx
+++ Cinemy/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
+import {getPopularMovies} from './services/services';
 
-const HelloWorldApp = () => {
-  const x = 'Hello World!';
-  console.log(x);
+const Cinemy = () => {
+  const [movie, setMovie] = useState(''); // Here we are using state variable so that the variable can be updated even after the template is rendered
+  const [errorMsg, setErrorMsg] = useState(false); // Stores error message(if any error occurs)
+
+  // useEffect helps us to update a dome variable after any specified time or updating it once
+  // Without useEffect, the variable asks for data unlimited times from the server, which cause more CPU usage
+  useEffect(() => {
+    // Here we use `then`, because async method return us a Promise that
+    // when it gets data, it must return them
+    getPopularMovies()
+      .then(movies => {
+        //console.log(movies[0]);
+        setMovie(movies[0]); // Initializing with the first movie of the movies list
+      })
+      .catch(error => {
+        setErrorMsg(error);
+      });
+  }, []); // Here we are using empty array as we want to update the variable `movie` only once
+
   return (
     <View
       style={{
@@ -11,8 +28,10 @@ const HelloWorldApp = () => {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <Text>Hello, world!</Text>
+      <Text>Movie Name: {movie.original_title}</Text>
+      <Text>Language: {movie.original_language}</Text>
+      {errorMsg && <Text style={{color: 'red'}}> Error in the server!!</Text>}
     </View>
   );
 };
-export default HelloWorldApp;
+export default Cinemy;
---
+++ Cinemy/services/services.tsx
@@ -0,0 +1,38 @@
+// -------- This file contains all the functions of the services the app will provide -------- //
+// ------------------------------------------------------------------------------------------- //
+
+import axios from 'axios'; // A service to grab data from an API
+
+const apiUrl = 'https://api.themoviedb.org/3';
+const apiKey = 'api_key=c62bc8c74953d6b00fe3df6282034a60';
+
+// ============== Get Popular Movies ============== //
+// As the getPopularMovies() function need to wait for the response, this must be asynchronous method
+// `export` is used beacuse we want to use this function in another file
+export const getPopularMovies = async () => {
+  // `await` tells to wait until response is ready
+  const response = await axios.get(
+    `${apiUrl}/movie/popular?${apiKey}`,
+  ); // Storing the response of GET request of the API call
+  //console.log(JSON.stringify(response.data, null, 2)); // Stylizing the response(filtering everything from response except data part)
+
+  return response.data.results; // Return the list of popular movies
+};
+
+// ============== Get Upcoming Movies ============== //
+export const getUpcomingMovies = async () => {
+  const response = await axios.get(
+    `${apiUrl}/movie/upcoming?${apiKey}`,
+  );
+
+  return response.data.results; // Return the list of upcoming movies
+};
+
+// ============== Get Popular TV series ============== //
+export const getPopularTVSeries = async () => {
+  const response = await axios.get(
+    `${apiUrl}/tv/popular?${apiKey}`,
+  );
+
+  return response.data.results; // Return the list of popular TV series
+};
