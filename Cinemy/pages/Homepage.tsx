cd616ce3e5d1ed83589d222800bd4ef24f1639da
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2023-02-13 12:48:02 +0600

    Refactoring code done. Promises to all service in one place added

--- Cinemy/pages/Homepage.tsx
+++ Cinemy/pages/Homepage.tsx
@@ -9,65 +9,52 @@ const slideshowImagePath = 'https://image.tmdb.org/t/p/w500';
 const dimensionScreen = Dimensions.get('screen'); // This returns the height & width of the device screen
 
 const Homepage = () => {
-    const [popularMovies, setPopularMovies] = useState(''); // Here we are using state variable so that the variable can be updated even after the template is rendered
-    const [familyMovies, setFamilyMovies] = useState('');
-    const [documentaryMovies, setDocumentaryMovies] = useState('');
-    const [popularTVShows, setPopularTVShows] = useState('');
-    const [moviesImages, setMoviesImages] = useState('');
+    const [popularMovies, setPopularMovies] = useState(); // Here we are using state variable so that the variable can be updated even after the template is rendered
+    const [familyMovies, setFamilyMovies] = useState();
+    const [documentaryMovies, setDocumentaryMovies] = useState();
+    const [popularTVShows, setPopularTVShows] = useState();
+    const [moviesImages, setMoviesImages] = useState();
     const [errorMsg, setErrorMsg] = useState(false); // Stores error message(if any error occurs)
+
+    // This will return promise to all the service functions.
+    // So, we don't need multiple `then` & `error` function.
+    // We can use only one `then` & `error` check.
+    const getMovies = () => {
+        return Promise.all([
+            getUpcomingMovies(),
+            getPopularMovies(),
+            getPopularTVShows(),
+            getFamilyMovies(),
+            getDocumentaryMovies(),
+        ]);
+    };
 
     // useEffect helps us to update a dome variable after any specified time or updating it once
     // Without useEffect, the variable asks for data unlimited times from the server, which cause more CPU usage
     useEffect(() => {
-        // Get Upcoming movies images for image slider show
-        getUpcomingMovies()
-        .then(movies => {
-            //console.log(movies[0]);
-            const moviesImagesArray = [];
-            movies.forEach(movie => {
-                moviesImagesArray.push(slideshowImagePath+movie.poster_path);
-            });
-            setMoviesImages(moviesImagesArray);
-        })
-        .catch(error => {
-            setErrorMsg(error);
-        });
-
-
         // Here we use `then`, because async method return us a Promise that
         // when it gets data, it must return them
-        getPopularMovies()
-        .then(movies => {
-            //console.log(movies[0]);
-            setPopularMovies(movies); // Initializing with the list of movies
-        })
-        .catch(error => {
-            setErrorMsg(error);
-        });
+        getMovies().then(
+            ([
+                upcomingMoviesData,
+                popularMoviesData,
+                popularTVShowsData,
+                familyMoviesData,
+                documentaryMoviesData,
+            ]) => {
+                // Get Upcoming movies images for image slider show
+                const moviesImagesArray = [];
+                upcomingMoviesData.forEach(movie => {
+                    moviesImagesArray.push(slideshowImagePath+movie.poster_path);
+                });
+                setMoviesImages(moviesImagesArray);
 
-        getPopularTVShows()
-        .then(movies => {
-            //console.log(movies[0]);
-            setPopularTVShows(movies); // Initializing with the list of movies
-        })
-        .catch(error => {
-            setErrorMsg(error);
-        });
-
-        getFamilyMovies()
-        .then(movies => {
-            //console.log(movies[0]);
-            setFamilyMovies(movies); // Initializing with the list of movies
-        })
-        .catch(error => {
-            setErrorMsg(error);
-        });
-
-        getDocumentaryMovies()
-        .then(movies => {
-            //console.log(movies[0]);
-            setDocumentaryMovies(movies); // Initializing with the list of movies
-        })
+                setPopularMovies(popularMoviesData);
+                setPopularTVShows(popularTVShowsData);
+                setFamilyMovies(familyMoviesData);
+                setDocumentaryMovies(documentaryMoviesData);
+            }
+        )
         .catch(error => {
             setErrorMsg(error);
         });
@@ -78,39 +65,59 @@ const Homepage = () => {
         // Fragment is used when we want multiple view under a single return
         <React.Fragment>
             <ScrollView>
-                <View
-                    style={styles.sliderContainer}>
-                    <SliderBox 
-                        images = {moviesImages} 
-                        dotStyle = {styles.sliderDotsHeight}
-                        sliderBoxHeight = {dimensionScreen.height / 1.5}
-                        autoplay = {true} 
-                        circleLoop = {true} />
-                </View>
-                <View 
-                    style = {styles.carousel}>
-                    <List //Passing arguments to `List` component
-                        title = "Populer Movies"
-                        content = {popularMovies}/> 
-                </View>
-                <View 
-                    style = {styles.carousel}>
-                    <List //Passing arguments to `List` component
-                        title = "Populer TV Shows"
-                        content = {popularTVShows}/> 
-                </View>
-                <View 
-                    style = {styles.carousel}>
-                    <List //Passing arguments to `List` component
-                        title = "Family Shows"
-                        content = {familyMovies}/> 
-                </View>
-                <View 
-                    style = {styles.carousel}>
-                    <List //Passing arguments to `List` component
-                        title = "Documentary Shows"
-                        content = {documentaryMovies}/> 
-                </View>
+                {/** Slideshow upcoming movies */}
+                {moviesImages && (
+                    <View
+                        style={styles.sliderContainer}>
+                        <SliderBox 
+                            images = {moviesImages} 
+                            dotStyle = {styles.sliderDotsHeight}
+                            sliderBoxHeight = {dimensionScreen.height / 1.5}
+                            autoplay = {true} 
+                            circleLoop = {true} />
+                    </View>
+                )}
+
+                {/** Popular Movies */}
+                {popularMovies && (
+                    <View 
+                        style = {styles.carousel}>
+                        <List //Passing arguments to `List` component
+                            title = "Populer Movies"
+                            content = {popularMovies}/> 
+                    </View>
+                )}
+
+                {/** Popular TV Shows */}
+                {popularTVShows && (
+                    <View 
+                        style = {styles.carousel}>
+                        <List //Passing arguments to `List` component
+                            title = "Populer TV Shows"
+                            content = {popularTVShows}/> 
+                    </View>
+                )}
+
+                {/** Family Movies */}
+                {familyMovies && (
+                    <View 
+                        style = {styles.carousel}>
+                        <List //Passing arguments to `List` component
+                            title = "Family Movies"
+                            content = {familyMovies}/> 
+                    </View>
+                )}
+
+                {/** Documentary Movies */}
+                {documentaryMovies && (
+                    <View 
+                        style = {styles.carousel}>
+                        <List //Passing arguments to `List` component
+                            title = "Documentary Movies"
+                            content = {documentaryMovies}/> 
+                    </View>
+                )}
+                
             </ScrollView>
         </React.Fragment>
     );
